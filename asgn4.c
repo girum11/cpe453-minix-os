66a2d340a44322bec7b202c93fddeeb82230726e
Girum Ibssa <ibssagirum@gmail.com>
2013-05-23 23:06:02 -0700

    First commit

---
+++ asgn4.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : asgn4.c
+ Author      : Girum Ibssa
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello, World!"); /* prints Hello, World! */
+	return EXIT_SUCCESS;
+}
---
+++ deploy.sh
@@ -0,0 +1,3 @@
+FILES="asgn4.c"
+
+scp $FILES gibssa@falcon.csc.calpoly.edu:~/cpe453/asgn4
